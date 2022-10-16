1b55bda2cb4944345fdbc974abcc06a250978b66
Dawn Walker <nosirrah.reklaw@gmail.com>
2022-10-16 22:19:16 +1100

    Update ark.js

--- ARK/src/js/ark.js
+++ ARK/src/js/ark.js
@@ -1,4 +1,4 @@
-var UwU = new Audio("https://www.myinstants.com/media/sounds/sussy-uwu.mp3")
+let UwU = new Audio("https://www.myinstants.com/media/sounds/sussy-uwu.mp3")
 function moan() {
 	UwU.play();
 	console.log("UwU");
